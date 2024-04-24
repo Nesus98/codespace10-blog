4d6af2699c5b4a6299c18d16ce6cd8d485b68b6c
Nesus98 <jesusarroyo30.98@gmail.com>
2024-04-25 00:11:23 +0200

    Modelo de datos

---
+++ src/Entity/Categoria.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoriaRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoriaRepository::class)]
+class Categoria
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $descripcion = null;
+
+    #[ORM\ManyToOne(inversedBy: 'categorias')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Espacio $espacio = null;
+
+    /**
+     * @var Collection<int, Entrada>
+     */
+    #[ORM\OneToMany(targetEntity: Entrada::class, mappedBy: 'categoria')]
+    private Collection $entradas;
+
+    public function __construct()
+    {
+        $this->entradas = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(?string $descripcion): static
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getEspacio(): ?Espacio
+    {
+        return $this->espacio;
+    }
+
+    public function setEspacio(?Espacio $espacio): static
+    {
+        $this->espacio = $espacio;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Entrada>
+     */
+    public function getEntradas(): Collection
+    {
+        return $this->entradas;
+    }
+
+    public function addEntrada(Entrada $entrada): static
+    {
+        if (!$this->entradas->contains($entrada)) {
+            $this->entradas->add($entrada);
+            $entrada->setCategoria($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEntrada(Entrada $entrada): static
+    {
+        if ($this->entradas->removeElement($entrada)) {
+            // set the owning side to null (unless already changed)
+            if ($entrada->getCategoria() === $this) {
+                $entrada->setCategoria(null);
+            }
+        }
+
+        return $this;
+    }
+}
