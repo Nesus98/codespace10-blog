07d5cd3f161cc498f02747ae9976ee7868506531
Nesus98 <jesusarroyo30.98@gmail.com>
2024-05-13 00:01:43 +0200

    filtrado

--- src/Controller/Api/EntradaController.php
+++ src/Controller/Api/EntradaController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class EntradaController extends AbstractController
 {
-    #[Route('/api/entrada', methods:['GET'])]
+    #[Route('/api/entrada', methods: ['GET'])]
     public function index(Request $request, EntradaRepository $entradaRepository, PaginatorInterface $paginator): Response
     {
         $currentPage = $request->query->get('page', 1);
@@ -19,16 +19,57 @@ class EntradaController extends AbstractController
         $query = $entradaRepository->getQueryByFilter($filter);
         $entradas = $paginator->paginate($query, $currentPage, 10);
         $resultado = [];
-        foreach($entradas as $entrada) {
+        foreach ($entradas as $entrada) {
             $resultado[] = [
                 'id' => $entrada->getId(),
                 'fecha' => $entrada->getFecha()->format('Y-m-d H:i:s'),
                 'slug' => $entrada->getSlug(),
                 'titulo' => $entrada->getTitulo(),
-                'usuario' => $entrada->getUsuario()->getEmail(), 
+                'usuario' => $entrada->getUsuario()->getEmail(),
                 'categoria' => $entrada->getCategoria()->getNombre()
             ];
         }
+
+        return $this->json($resultado);
+    }
+
+    #[Route('/api/entrada{slug}', methods: ['GET'])]
+    public function detail($slug, EntradaRepository $entradaRepository)
+    {  
+       $entrada = $entradaRepository->findOneBy(['slug' => $slug]);
+        if($entrada == null) {
+            throw $this->createNotFoundException();
+        }
+
+        // Comentarios
+        $comentarios = $entrada->getComentarios();
+        $resultadoComentarios = [];
+        foreach($comentarios as $comentario) {
+            $resultadoComentarios[] = [
+                'fecha' => $comentario->getFecha()->format('Y-m-d H:i:s'),
+                'usuario' => $comentario->getUsuario()->getEmail(),
+                'texto' => $comentario->getTexto()
+            ];
+        }
+
+        //Etiquetas
+        $etiquetas = $entrada->getEtiquetas();
+        $resultadoEtiquetas = [];
+        foreach ($etiquetas as $etiqueta) {
+            $resultadoEtiquetas[] = $etiqueta->getNombre();
+        }
+
+    $resultado = [
+        'titulo' => $entrada->getTitulo(),
+        'fecha' => $entrada->getFecha()->format('Y-m-d H:i:s'),
+        'usuario' => $entrada->getUsuario()->getEmail(),
+        'categoria' => $entrada->getCategoria()->getNombre(),
+        'espacio' => $entrada->getCategoria()->getEspacio()->getNombre(),
+        'resumen' => $entrada->getResumen(),
+        'texto' => $entrada->getTexto(),
+        'comentarios' => $resultadoComentarios,
+        'etiquetas' => $resultadoEtiquetas
+    ];
 
         return $this->json($resultado);
     }
