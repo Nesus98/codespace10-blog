2a39c77677bbfe13ea6334298af033886a376938
Nesus98 <jesusarroyo30.98@gmail.com>
2024-05-12 15:56:56 +0200

    token refreshd

---
+++ src/Controller/Api/EntradaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\EntradaRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class EntradaController extends AbstractController
+{
+    #[Route('/api/entrada', methods:['GET'])]
+    public function index(Request $request, EntradaRepository $entradaRepository, PaginatorInterface $paginator): Response
+    {
+        $currentPage = $request->query->get('page', 1);
+        $query = $entradaRepository->getQueryAll();
+        $entradas = $paginator->paginate($query, $currentPage, 10);
+        $entradas = $entradaRepository->findAll();
+        $resultado = [];
+        foreach($entradas as $entrada) {
+            $resultado[] = [
+                'id' => $entrada->getId(),
+                'fecha' => $entrada->getFecha()->format('Y-m-d H:i:s'),
+                'slug' => $entrada->getSlug(),
+                'titulo' => $entrada->getTitulo()
+            ];
+        }
+
+        return $this->json($resultado);
+    }
+}
